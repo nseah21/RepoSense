141124f523a4ce1d408951ae8a86bfd19a70cf47
nseah21 <nseah21@gmail.com>
2023-07-01 00:02:34 +0800

    Further refactor build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.api.tasks.Exec
+import org.gradle.api.file.DuplicatesStrategy
+import org.gradle.api.plugins.quality.Checkstyle
+import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
+import org.gradle.api.tasks.testing.Test
 
 plugins {
     application
@@ -13,17 +17,28 @@ plugins {
     id("com.palantir.git-version") version "0.13.0"
 }
 
-node {
-    setDownload(false)
-}
+val os: OperatingSystem = DefaultNativePlatform.getCurrentOperatingSystem()
 
 application {
     mainClassName = "reposense.RepoSense"
 }
 
+node {
+    setDownload(false)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 repositories {
     mavenCentral()
 }
+
+/* Still need to extend from testImplementation and testRuntime */
+val systemtestImplementation = configurations.create("systemtestImplementation")
+val systemtestRuntime = configurations.create("systemtestRuntime")
 
 dependencies {
     val jUnitVersion: String = "5.8.2"
@@ -44,10 +59,159 @@ val installFrontend = tasks.register<Exec>("installFrontend") {
     setArgs(listOf("ci"))
 }
 
-tasks.register<Exec>("buildFrontend") {
+val buildFrontend = tasks.register<Exec>("buildFrontend") {
+    setDependsOn(listOf(installFrontend))
+    setCommandLine("cmd")
+
+    setWorkingDir("frontend/")
+    setArgs(listOf("run", "devBuild"))
+}
+
+val zipReport = tasks.register<Zip>("zipReport") {
+    setDependsOn(listOf(buildFrontend))
+    from("frontend/build/")
+
+    archiveBaseName.set("templateZip")
+    destinationDirectory.set(file("src/main/resources"))
+}
+
+tasks.register<Copy>("copyCypressConfig") {
+    description = "Copies the config files used by the backend to generate the test report for Cypress testing into an isolated working directory"
+
+    from("frontend/cypress/config/")
+    into("build/serveTestReport/exampleconfig/")
+}
+
+tasks.register<Copy>("copyMainClasses") {
+    description = "Copies the backend classes used to generate the test report for Cypress testing into an isolated working directory"
+    // setDependsOn(listOf(classes))
+
+    from("build/classes/java/main/")
+    into("build/serveTestReport/java/main/")
+}
+
+//val compileJava = tasks.compileJava
+
+// tasks.named<ProcessResources>("processSystemtestResources").configure {
+//     duplicatesStrategy = DuplicatesStrategy.INCLUDE
+// }
+
+tasks.named("run") {
+    dependsOn(zipReport)
+    tasks.compileJava {
+        mustRunAfter(zipReport)
+    }
+//    args = System.getProperty("args", "").split("")
+//    systemProperty("version", getRepoSenseVersion())
+}
+
+checkstyle {
+    toolVersion = "9.3"
+    getConfigDirectory().set(file("${rootProject.projectDir}/config/checkstyle"))
+}
+
+//idea {
+//    module {
+//        sourceSets {
+//            named("systemtest") {
+//                allSource.srcDirs.forEach { srcDir ->
+//                    testSourceDirs.plusAssign(srcDir)
+//                }
+//            }
+//        }
+//    }
+//}
+
+//tasks.test {
+//    systemProperty("REPOSENSE_ENVIRONMENT", "TEST")
+//
+//    testLogging {
+//        events("passed", "skipped", "failed")
+//        showStandardStreams = true
+//    }
+//
+////    doFirst {
+////        deleteReposAddressDirectory()
+////    }
+//
+//    useJUnitPlatform()
+//
+////    doLast {
+////        deleteReposAddressDirectory()
+////    }
+//}
+
+tasks.shadowJar {
+    dependsOn(zipReport)
+}
+
+tasks.named<ShadowJar>("shadowJar") {
+//    tasks.compileJava {
+//        mustRunAfter("zipReport")
+//    }
+//    tasks.processResources {
+//        mustRunAfter("zipReport")
+//    }
+    archiveFileName.set("RepoSense.jar")
+    destinationDirectory.set(file("${buildDir}/jar/"))
+
+    manifest {
+//        getReposenseVersion()
+    }
+}
+
+tasks.register<Exec>("lintFrontend") {
     setDependsOn(listOf(installFrontend))
     setCommandLine("npm.cmd")
 
     setWorkingDir("frontend/")
-    setArgs(listOf("run", "devBuild"))
+    setArgs(listOf("run", "lint"))
+}
+
+val checkstyleMain = tasks.checkstyleMain
+val checkstyleTest = tasks.checkstyleTest
+// val checkstyleSystemtest = tasks.checkstyleSystemtest
+
+tasks.register<Checkstyle>("checkstyleAll") {
+    setDependsOn(listOf(checkstyleMain, checkstyleTest)) //, checkstyleSystemtest))
+    tasks.checkstyleTest {
+        mustRunAfter(checkstyleMain)
+    }
+//    tasks.checkstyleSystemtest {
+//        mustRunAfter(checkstyleTest)
+//    }
+}
+
+tasks.register<Exec>("environmentalChecks") {
+    setWorkingDir("config/checks/")
+    if (os.isWindows()) {
+        setCommandLine(listOf("cmd", "/c", "run-checks.bat"))
+    } else {
+        setCommandLine(listOf("sh", "./run-checks.sh"))
+    }
+}
+
+tasks.register<Test>("systemtest") {
+    testClassesDirs = sourceSets.getByName("systemtest").output.classesDirs
+    classpath = sourceSets.getByName("systemtest").runtimeClasspath
+
+    systemProperty("REPOSENSE_ENVIRONMENT", "TEST")
+
+    testLogging {
+        events("passed", "skipped", "failed")
+        showStandardStreams = true
+    }
+//    doFirst {
+//        deleteReposAddressDirectory()
+//    }
+
+    useJUnitPlatform()
+
+//    doLast {
+//        deleteReposAddressDirectory()
+//    }
+}
+
+val serveTestReportInBackground = tasks.register("") {
+
 }
